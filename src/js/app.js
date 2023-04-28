3f2870aaefe1448e8f426083a344b436b6c6e04e
Elena Spirina <espirina@list.ru>
2023-04-28 07:34:02 +0400

    corrections

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,4 @@ console.log('app worked');
 const game = new Game();
 game.start();
 
-import Game, { GameSavingData } from '/.game.js/';
-
-import loadGame, { readGameSaving as loadGame } from '/.game.js/';
-
-import saveGame, { writeGameSaving as saveGame } from '/.game.js/';
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from '/.game.js/';
